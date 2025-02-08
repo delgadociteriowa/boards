e55c2ca8a664ef83f6ea0329131263b5de0639a4
Carlos <delgadociteriowa@gmail.com>
2025-02-08 14:35:48 -0300

    board places base

--- src/pages/Chess/Chess.jsx
+++ src/pages/Chess/Chess.jsx
@@ -1,6 +1,23 @@
+import React, { useState } from 'react';
+
 import './Chess.css';
 
+{
+  /* <div className='chess__place'></div> */
+}
+
 const Chess = () => {
+  const [chessGrid, setChessGrid] = useState(() => {
+    const grid = [];
+    for (let i = 1; i <= 12; i++) {
+      for (let j = 1; j <= 8; j++) {
+        grid.push({ id: `${i}-${j}`, piece: '' });
+      }
+    }
+    console.log(grid);
+    return grid;
+  });
+
   return (
     <>
       <nav className='nav nav--game'>
@@ -14,177 +31,51 @@ const Chess = () => {
       <section className='board__chess'>
         <div className='board__chess__container'>
           <div className='board__chess__grid'>
-            <div className='chess__place chess__discard da1 corner-1'></div>
-            <div className='chess__place chess__discard da2'></div>
-            <div className='chess__place chess__discard da3'></div>
-            <div className='chess__place chess__discard da4'></div>
-            <div className='chess__place chess__discard da5'></div>
-            <div className='chess__place chess__discard da6'></div>
-            <div className='chess__place chess__discard da7'></div>
-            <div className='chess__place chess__discard da8 corner-2'></div>
-
-            <div className='chess__place chess__discard db1'></div>
-            <div className='chess__place chess__discard db2'></div>
-            <div className='chess__place chess__discard db3'></div>
-            <div className='chess__place chess__discard db4'></div>
-            <div className='chess__place chess__discard db5'></div>
-            <div className='chess__place chess__discard db6'></div>
-            <div className='chess__place chess__discard db7'></div>
-            <div className='chess__place chess__discard db8'></div>
-
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece black__piece'>♜</span>
-            </div>
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece black__piece'>♞</span>
-            </div>
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece black__piece'>♝</span>
-            </div>
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece black__piece'>♛</span>
-            </div>
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece black__piece'>♚</span>
-            </div>
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece black__piece'>♝</span>
-            </div>
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece black__piece'>♞</span>
-            </div>
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece black__piece'>♜</span>
-            </div>
-
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece black__piece'>♟</span>
-            </div>
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece black__piece'>♟</span>
-            </div>
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece black__piece'>♟</span>
-            </div>
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece black__piece'>♟</span>
-            </div>
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece black__piece'>♟</span>
-            </div>
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece black__piece'>♟</span>
-            </div>
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece black__piece'>♟</span>
-            </div>
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece black__piece'>♟</span>
-            </div>
-
-            <div className='chess__place chess__square white__square'></div>
-            <div className='chess__place chess__square black__square'></div>
-            <div className='chess__place chess__square white__square'></div>
-            <div className='chess__place chess__square black__square'></div>
-            <div className='chess__place chess__square white__square'></div>
-            <div className='chess__place chess__square black__square'></div>
-            <div className='chess__place chess__square white__square'></div>
-            <div className='chess__place chess__square black__square'></div>
-
-            <div className='chess__place chess__square black__square'></div>
-            <div className='chess__place chess__square white__square'></div>
-            <div className='chess__place chess__square black__square'></div>
-            <div className='chess__place chess__square white__square'></div>
-            <div className='chess__place chess__square black__square'></div>
-            <div className='chess__place chess__square white__square'></div>
-            <div className='chess__place chess__square black__square'></div>
-            <div className='chess__place chess__square white__square'></div>
-
-            <div className='chess__place chess__square white__square'></div>
-            <div className='chess__place chess__square black__square'></div>
-            <div className='chess__place chess__square white__square'></div>
-            <div className='chess__place chess__square black__square'></div>
-            <div className='chess__place chess__square white__square'></div>
-            <div className='chess__place chess__square black__square'></div>
-            <div className='chess__place chess__square white__square'></div>
-            <div className='chess__place chess__square black__square'></div>
-
-            <div className='chess__place chess__square black__square'></div>
-            <div className='chess__place chess__square white__square'></div>
-            <div className='chess__place chess__square black__square'></div>
-            <div className='chess__place chess__square white__square'></div>
-            <div className='chess__place chess__square black__square'></div>
-            <div className='chess__place chess__square white__square'></div>
-            <div className='chess__place chess__square black__square'></div>
-            <div className='chess__place chess__square white__square'></div>
-
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece white__piece'>♟</span>
-            </div>
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece white__piece'>♟</span>
-            </div>
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece white__piece'>♟</span>
-            </div>
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece white__piece'>♟</span>
-            </div>
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece white__piece'>♟</span>
-            </div>
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece white__piece'>♟</span>
-            </div>
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece white__piece'>♟</span>
-            </div>
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece white__piece'>♟</span>
-            </div>
-
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece white__piece'>♜</span>
-            </div>
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece white__piece'>♞</span>
-            </div>
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece white__piece'>♝</span>
-            </div>
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece white__piece'>♛</span>
-            </div>
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece white__piece'>♚</span>
-            </div>
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece white__piece'>♝</span>
-            </div>
-            <div className='chess__place chess__square black__square'>
-              <span className='chess__piece white__piece'>♞</span>
-            </div>
-            <div className='chess__place chess__square white__square'>
-              <span className='chess__piece white__piece'>♜</span>
-            </div>
-
-            <div className='chess__place chess__discard dc1'></div>
-            <div className='chess__place chess__discard dc2'></div>
-            <div className='chess__place chess__discard dc3'></div>
-            <div className='chess__place chess__discard dc4'></div>
-            <div className='chess__place chess__discard dc5'></div>
-            <div className='chess__place chess__discard dc6'></div>
-            <div className='chess__place chess__discard dc7'></div>
-            <div className='chess__place chess__discard dc8'></div>
-
-            <div className='chess__place chess__discard dd1 corner-3'></div>
-            <div className='chess__place chess__discard dd2'></div>
-            <div className='chess__place chess__discard dd3'></div>
-            <div className='chess__place chess__discard dd4'></div>
-            <div className='chess__place chess__discard dd5'></div>
-            <div className='chess__place chess__discard dd6'></div>
-            <div className='chess__place chess__discard dd7'></div>
-            <div className='chess__place chess__discard dd8 corner-4'></div>
+            {chessGrid.map((place, i) => {
+              if (i < 16) {
+                return (
+                  <div key={place.id} class='chess__place chess__discard'></div>
+                );
+              } else if (i > 79) {
+                return (
+                  <div key={place.id} class='chess__place chess__discard'></div>
+                );
+              } else {
+                if (place.id.split('-')[0] % 2) {
+                  if (place.id.split('-')[1] % 2) {
+                    return (
+                      <div
+                        key={place.id}
+                        class='chess__place white__square'
+                      ></div>
+                    );
+                  } else {
+                    return (
+                      <div
+                        key={place.id}
+                        class='chess__place black__square'
+                      ></div>
+                    );
+                  }
+                } else {
+                  if (place.id.split('-')[1] % 2) {
+                    return (
+                      <div
+                        key={place.id}
+                        class='chess__place black__square'
+                      ></div>
+                    );
+                  } else {
+                    return (
+                      <div
+                        key={place.id}
+                        class='chess__place white__square'
+                      ></div>
+                    );
+                  }
+                }
+              }
+            })}
           </div>
           <div className='board__chess__options'>
             <button className='option_button'>New Game</button>
